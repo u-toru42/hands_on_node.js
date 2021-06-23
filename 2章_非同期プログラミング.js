0ebecdd515251328c583d395edcfb2000c9ebf78
u-toru42 <golt7040@gmail.com>
2021-06-23 20:31:25 +0900

    同期・非同期の書き分けでParseJSONAsyncWithCacheを書き直し

--- 2章_非同期プログラミング.js
+++ 2章_非同期プログラミング.js
@@ -1,5 +1,3 @@
-const { fstat } = require("fs")
-
 // 2.2.1 コールバックを利用した非同期APIを実行する
 setTimeout(
   () => console.log('1秒経過しました'), // コールバック
@@ -89,3 +87,52 @@ function parseJSONAsyncWithCache(json, callback) {
     callback(err, result)
   })
 }
+
+// 1回目の実行
+parseJSONAsyncWithCache(
+  '{"message": "Hello", "to": "World"}',
+  (err, result) => {
+    console.log('1回目の結果', err, result)
+    // コールバックの中で2回めを実行
+    parseJSONAsyncWithCache(
+      '{"message": "Hello", "to": "World"}',
+      (err, result) => {
+        console.log('2回目の結果', err, result)
+      }
+    )
+    console.log('2回目の呼び出し完了')
+  }
+)
+console.log('1回目の呼び出し完了');
+
+// コールバックをパラメータとする関数は、同期的か非同期的かのどちらかで実装する
+const cache2 = {}
+function parseJSONAsyncWithCache(json, callback) {
+  const cached = cached[json]
+  if (cached) {
+    // キャッシュに値が存在する場合でも、非同期的にコールバックを実行する
+    setTimeout(() => callback(cached.err, cached.result), 0)
+    return
+  }
+  parseJSONAsync(json, (err, result) => {
+    cache2[json] = { err, result }
+    callback(err, result)
+  })
+}
+
+// 1回目の実行
+parseJSONAsyncWithCache(
+  '{"message": "Hello", "to": "World"}',
+  (err, result) => {
+    console.log('1回目の結果', err, result)
+    // コールバックの中で2回目を実行
+    parseJSONAsyncWithCache(
+      '{"message": "Hello", "to": "World"}',
+      (err, result) => {
+        console.log('2回目の結果', err, result)
+      }
+    )
+    console.log('2回目の呼び出し完了')
+  }
+)
+console.log('1回目の呼び出し完了');
