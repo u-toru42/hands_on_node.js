89507122c23aa8f7c71a6c46e041cdad9b5770da
u-toru42 <golt7040@gmail.com>
2021-06-24 20:29:55 +0900

    p59途中まで

--- 2章_非同期プログラミング.js
+++ 2章_非同期プログラミング.js
@@ -1,3 +1,5 @@
+const { threadId } = require("worker_threads")
+
 // 2.2.1 コールバックを利用した非同期APIを実行する
 setTimeout(
   () => console.log('1秒経過しました'), // コールバック
@@ -138,6 +140,7 @@ function parseJSONAsyncWithCache(json, callback) {
   parseJSONAsync(json, (err, result) => {
     cache2[json] = { err, result }
     callback(err, result)
+
   })
 }
 
@@ -157,3 +160,119 @@ parseJSONAsyncWithCache(
   }
 )
 console.log('1回目の呼び出し完了');
+
+// 2.2.3.1 コールバックの実行を非同期化するのに使用するAPI
+
+// テスト用でparseJSONAsyncを再定義
+
+// parseJSONAsync()をNode.jsの規約に沿って書き直すと次のようになる
+function parseJSONAsync(json, callback) {
+  setTimeout(() => {
+    try {
+      callback(null, JSON.parse(json))
+    } catch (err) {
+      callback(err)
+    }
+  }, 1000)
+}
+parseJSONAsync('不正なJSON',
+  (err, result) => console.log('parse結果', err, result)
+)
+
+
+const cache3 = {}
+function parseJSONAsyncWithCache(json, callback) {
+  const cached = cache3[json]
+  if (cached) {
+    // Node.jsのみを対象としたコードの場合
+    process.nextTick(() => callback(cached.err, cached.result))
+    // ブラウザ環境でも動かすコードの場合
+    // 1. queueMicrotask()を使う
+    // queueMicrotask(() => callback(cached.err, cached.result))
+    // 2. Promiseを使う
+    // Promise.resolve().then(() => callback(cached.err, cached.result))
+    return
+  }
+  parseJSONAsync(json, (err, result) => {
+    cache3[json] = { err, result }
+    callback(err, result)
+  })
+}
+
+// 1回目の実行
+parseJSONAsyncWithCache(
+  '{"message": "Hello", "to": "World"}',
+  (err, result) => {
+    console.log('1回目の結果', err, result)
+    // コールバックの中で2回めを実行
+    parseJSONAsyncWithCache(
+      '{"message": "Hello", "to": "World"}',
+      (err, result) => {
+        console.log('2回目の結果', err, result)
+      }
+    )
+    console.log('2回目の呼び出し完了')
+  }
+)
+console.log('1回目の呼び出し完了')
+
+// 2.2.4 コールバックヘル
+
+asyncFunc1(input, (err, result) => {
+  if (err) {
+    // エラーハンドリング
+  }
+  asyncFunc2(result, (err, result) => {
+    if (err) {
+      // エラーハンドリング
+    }
+    asyncFunc3(result, (err, result) => {
+      if (err) {
+        // エラーハンドリング
+      }
+      asyncFunc4(result, (err, result) => {
+        if (err) {
+          // エラーハンドリング
+        }
+      // ...
+      })
+    })
+  })
+})
+
+// コードを分割する
+
+function first(arg, callback) {
+  asyncFunc1(arg, (err, result) => {
+    if (err) {
+      return callback(err)
+    }
+    ScriptProcessorNode(result, callback)
+  })
+}
+
+function second(arg, callback) {
+  asyncFunc2(arg, (err, result) => {
+    if (err) {
+      return callback(err)
+    }
+    threadId(result, callback)
+  })
+}
+
+function third(arg, callback) {
+  asyncFunc3(arg, (err, result) => {
+    if (err) {
+      return callback(err)
+    }
+    asyncFunc4(result, callback)
+  })
+}
+
+// すべての非同期処理を実行する
+first(input, (err, result) => {
+  if (err) {
+    // エラーハンドリング
+  }
+  // ...
+})
