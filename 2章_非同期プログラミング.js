13313e818c6390293650abcb7279fd60e46afb0f
u-toru42 <golt7040@gmail.com>
2021-07-03 19:55:44 +0900

    async/awaitの途中まで

--- 2章_非同期プログラミング.js
+++ 2章_非同期プログラミング.js
@@ -612,3 +612,282 @@ Promise.any([]).catch(err => console.log(err.errors))
 // 2.3.3.5 ショートサーキットの条件による各メソッドの分類
 
 // 2.3.4 Promiseのメリット
+
+const readdir = util.promisify(fs.readdir)
+
+readdir('.').then(console.log)
+
+setTimeout[util.promisify.custom]
+
+const setTimeoutPromise = util.promisify(setTimeout)
+
+setTimeoutPromise(1000).then(() => console.log('1秒経過'))
+
+fs.promises.readdir('.').then(console.log)
+
+// 2.4.1 ジェネレータの生成
+/**
+ * ジェネレータ関数
+ */
+
+function* generatorFunc() {
+  yield 1
+  yield 2
+  yield 3
+}
+
+// ジェネレータの生成
+const generator = generatorFunc()
+
+function* generatorFunc() {
+  console.log('ジェネレータ関数開始')
+  console.log('yield 1')
+  yield 1
+  console.log('yield 2')
+  yield 2
+  console.log('ジェネレータ関数終了')
+  return 'ジェネレータ関数戻り値'
+}
+
+const generator2 = generatorFunc()
+
+const iterator = generator2[Symbol.iterator]()
+
+iterator.next()
+
+iterator === generator2
+
+const generator3 = generatorFunc()
+
+for (const v of generator3) {
+  console.log('for...of', v)
+}
+
+const arrayIterator = [1, 2, 3][Symbol.iterator]()
+
+arrayIterator.next()
+
+// 2.4.3 引数を渡したnext()の実行、およびthrow()
+
+// リセット可能なカウンタを実装するジェネレータ関数
+function* resetableGeneratorFunc() {
+  let count = 0
+  while (true) {
+    // next()を真に評価される引数(trueなど)で実行すると、
+    // ここでカウンタがリセットされる
+    if (yield count++) {
+      count = 0
+    }
+  }
+}
+
+const resetableGenerator = resetableGeneratorFunc()
+
+resetableGenerator.next()
+
+resetableGenerator.next(true)
+
+
+function* tryCatchGeneratorFunc() {
+  try {
+    yield 1
+  } catch (err) {
+    console.log('エラーをキャッチ', err)
+    yield 2
+  }
+}
+
+const tryCatchGenerator = tryCatchGeneratorFunc()
+
+tryCatchGenerator.next()
+
+tryCatchGenerator.throw(new Error('エラー'))
+
+tryCatchGenerator.next()
+
+try {
+  // try...catchのないresetableGeneratorに対してthrow()を実行
+  resetableGenerator.throw(new Error('エラー'))
+} catch (err) {
+  console.log('ジェネレータ外でエラーをキャッチ', err)
+}
+
+resetableGenerator.next()
+
+// 2.4.4 ジェネレータを利用した非同期プログラミング
+
+function parseJSONAsync(json) {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+      resolve(JSON.parse(json))
+      } catch (err) {
+        reject(err)
+    }
+    }, 1000)
+  )
+}
+
+// yieldの仕組みを利用して非同期処理を実行する関数
+function* asyncWithGeneratorFunc(json) {
+  try {
+    // 非同期処理の実行
+    const result = yield parseJSONAsync(json)
+    console.log('パース結果', result)
+  } catch (err) {
+    console.log('エラーをキャッチ', err)
+  }
+}
+
+// 正常系
+const asyncWithGenerator1 = asyncWithGeneratorFunc('{ "foo": 1 }')
+
+// "yield parseJSONAsync(json)"で生成されるPromiseインスタンスの取得
+const promise1 = asyncWithGenerator1.next().value
+
+// Promiseインスタンスが解決された値をnext()メソッドに渡す
+promise1.then(result => asyncWithGenerator1.next(result))
+
+// 異常系
+const asyncWithGenerator2 = asyncWithGeneratorFunc('不正なJSON')
+
+// "yield parseJSONAsync(json)"で生成されるPromiseインスタンスの取得
+const promise2 = asyncWithGenrator2.next().value
+
+promise2.catch(err => asyncWithGenerator2.throw(err))
+
+// 非同期処理を実行するジェネレータの汎用的なハンドリング関数
+// 戻り値はPromiseインスタンス
+function handleAsyncWithGenerator(generator, resolved) {
+  // 前回yieldされたPromiseインスタンスの値を引数にnext()を実行
+  // 初回はresolvedには値が入っていない(undefined)
+  const { done, value } = generator.next(resolved)
+  if (done) {
+    // ジェネレータが完了した場合はvalueで解決されるPromiseインスタンスを返す
+    return Promise.resolve(value)
+  }
+  return value.then(
+    // 正常系では再起呼び出し
+    resolved => handleAsyncWithGenerator(generator, resolved),
+    // 異常系ではthrow()メソッドを実行
+    err => generator.throw(err)
+  )
+}
+
+handleAsyncWithGenerator(asyncWithGeneratorFunc('{ "foo": 1}'))
+handleAsyncWithGenerator(asyncWithGeneratorFunc('不正なJSON'))
+
+// 2.5 async/await
+
+async function asyncFunc(input) {
+  try {
+    const result1 = await asyncFunc1(input)
+    const result2 = await asyncFunc2(input)
+    const result3 = await asyncFunc3(input)
+    const result4 = await asyncFunc4(input)
+
+  } catch (e) {
+    // エラーハンドリング
+  }
+}
+
+// 実際に使ってみましょう
+// 非同期的にJSONをパースする関数(再掲)
+function parseJSONAsync(json) {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+      resolve(JSON.parse(json))
+      } catch (err) {
+        reject(err)
+    }
+    }, 1000)
+  )
+}
+
+// async/await構文を利用した非同期処理
+async function asyncFunc(json) {
+  try {
+    const result = await parseJSONAsync(json)
+    console.log('パース結果', result)
+  } catch (err) {
+    console.log('エラーをキャッチ', err)
+  }
+}
+
+// 正常系
+asyncFunc('{ "foo": 1}')
+
+// 異常系
+asyncFunc('不正なJSON')
+
+// asyncキーワードの着いた関数(async関数)は必ずPromiseインスタンスを返します。
+async function asyncReturnFoo() {
+  return 'foo'
+}
+
+asyncReturnFoo()
+
+// async関数内でエラーが投げられると、async関数はrejectedなPromiseインスタンスを返します。
+async function asyncThrow() {
+  throw new Error('エラー')
+}
+
+asyncThrow()
+
+// ジェネレータ関数におけるyieldと同様、awaitは
+// async関数内の処理を一時呈するものの、非同期I / Oのように
+// スレッドの処理をブロックするわけではありません。
+
+async function pauseAndResume(pausePeriod) {
+  console.log('pauseAndResume開始')
+  await new Promise(resolve => setTimeout(resolve, pausePeriod))
+  console.log('pauseAndResume再開')
+}
+
+pauseAndResume(1000)
+console.log('async関数外の処理はawaitの影響を受けない')
+
+async function doSomethingAsyncConcurrently() {
+  // 複数の非同期処理の並列実行
+  const result = await Promise.all([
+    asyncFunc1(),
+    asyncFunc2(),
+    asyncFunc3()
+  ])
+}
+
+// 2.5.4 トップレベルawait
+
+(async () => {
+  // 非同期に値を取得
+  const a = await getSomethingAsync()
+})()
+
+const asyncIterable = {
+  [Symbol.asyncIterator]() {
+    let i = 0
+    // asyncイテレータ
+    return {
+      // value, doneプロパティを持つオブジェクトで解決されるPromiseを返す
+      next() {
+        if (i > 3) {
+          return Promise.resolve({ done: true })
+        }
+        return new Promise(resolve => setTimeout(
+          () => resolve({ value: i++, done: false }),
+          100
+        ))
+      }
+    }
+  }
+}
+
+for await (const element of asyncIterable) {
+  console.log(element)
+}
+
+// asyncジェネレータ関数
+async function* asyncGenerator() {
+  let i = 0
+}
